0bd9cc2c031422efc8c832961963e1e0cf29c86a
leeharyung <lhr4884@naver.com>
2023-01-23 18:57:24 +0900

    docs: 강의실 배정 multiset solution

--- 5주차/강의실 배정/이하령.cpp
+++ 5주차/강의실 배정/이하령.cpp
@@ -35,3 +35,41 @@ int main() {
     }
     printf("%d", ans);
 }
+
+// sol 2) multiset
+
+// #include <iostream>
+// #include <cstdio>
+// #include <algorithm>
+// #include <queue>
+// #include <deque>
+// #include <set>
+// using namespace std;
+// typedef pair<int, int> pii;
+// typedef pair<pii, int> piii;
+
+// int n;
+// multiset<pii> mp;
+
+// int main() {
+//     int ans = 0;
+//     scanf("%d", &n);
+//     for(int i = 0; i < n; i++) {
+//         int a, b;
+//         scanf("%d %d", &a, &b);
+//         mp.insert({a, b});
+//     }
+
+//     while(!mp.empty()) {
+//         auto it = mp.begin(); ans++;
+//         pii itValue = *mp.begin();
+
+//         mp.erase(it);
+//         while(mp.lower_bound({itValue.second, -1}) != mp.end()) {
+//             it = mp.lower_bound({itValue.second, -1});
+//             itValue = *it;
+//             mp.erase(it);
+//         }
+//     }
+//     printf("%d", ans);
+// }
