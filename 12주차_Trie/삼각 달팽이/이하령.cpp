cc137ef4d241be58dd9fe5203ef9b3680af6318f
leeharyung <lhr4884@naver.com>
2023-03-13 22:45:12 +0900

    docs: add 삼각 달팽이

---
+++ 12주차_Trie/삼각 달팽이/이하령.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+bool isFirst = true;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    int cnt = (n * (n+1)) / 2, tmp = 1;
+    int arr[n][n], x = 0, y = 0, le = 0, ri = n-1, to = 0, bo = n-1;
+    while(1) {
+        x = to;
+        while(cnt > 0) {
+            arr[x][y] = tmp++;
+            x++;
+            cnt--;
+            if (x >= bo) break;
+        }
+        y = le++;
+        to = isFirst ? to + 1 : to + 2;
+
+        while(cnt > 0) {
+            arr[x][y] = tmp++;
+            y++;
+            cnt--;
+            if (y >= ri) break;
+        }
+        isFirst = false;
+        while(cnt > 0) {
+            arr[x][y] = tmp++;
+            x--; y--;
+            cnt--;
+            if (x <= to) break;
+        }
+        ri = isFirst ? ri - 1 : ri - 2;
+
+        bo--;
+        if (cnt <= 0) break;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= i; j++) {
+            answer.push_back(arr[i][j]);
+        }
+    }
+    return answer;
+}
+
+int main() {
+    int n = 9;
+    vector<int> v = solution(n);
+    for(int i : v) printf("%d ", i);
+}
