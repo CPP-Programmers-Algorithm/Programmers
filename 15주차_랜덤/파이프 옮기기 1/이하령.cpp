f0be52f8a071d9da2c9f5c576d2e93a05b1582bb
leeharyung <lhr4884@naver.com>
2023-04-03 15:39:22 +0900

    docs: add 파이프 옮기기

---
+++ 15주차_랜덤/파이프 옮기기 1/이하령.cpp
@@ -0,0 +1,88 @@
+#include <cstdio>
+using namespace std;
+
+int dy[] = {0, 1, 1, -1, 0}, dx[] = {1, 0,  1, 0, -1};
+struct axis { int x, y; };
+
+int n;
+int arr[16][16];
+int ans = 0;
+
+void go(axis cur, int mode) { // 0: 아래, 1: 오, 2: 대각선
+    if (cur.x == n-1 and cur.y == n-1) {
+        ans++;
+        return;
+    } else {
+        if (mode == 0) {
+            for (int i = 0; i < 3; i++) {
+                if (i == 1) continue;
+                int x = cur.x + dx[i], y = cur.y + dy[i];
+                if (x >= n or y >= n) continue;
+                if (arr[x][y]) continue;
+                bool check = true;
+                if (i == 2) {
+                    for(int j = 3; j < 5; j++) {
+                        int xx = x + dx[j], yy = y + dy[j];
+                        if (xx < 0 or yy < 0 or xx >= n or yy >= n or arr[xx][yy]) {
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+                if (check) {
+                    go({x, y}, i);
+                }
+            }
+        } else if (mode == 1) {
+            for (int i = 2; i >= 1; i--) {
+                int x = cur.x + dx[i], y = cur.y + dy[i];
+                if (x >= n or y >= n) continue;
+                if (arr[x][y]) continue;
+                bool check = true;
+                if (i == 2) {
+                    for(int j = 3; j < 5; j++) {
+                        int xx = x + dx[j], yy = y + dy[j];
+                        if (xx < 0 or yy < 0 or xx >= n or yy >= n or arr[xx][yy]) {
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+                if (check) {
+                    go({x, y}, i);
+                }
+            }
+        } else {
+            for (int i = 0; i < 3; i++) {
+                int x = cur.x + dx[i], y = cur.y + dy[i];
+                if (x >= n or y >= n) continue;
+                if (arr[x][y]) continue;
+                bool check = true;
+                if (i == 2) {
+                    for(int j = 3; j < 5; j++) {
+                        int xx = x + dx[j], yy = y + dy[j];
+                        if (xx < 0 or yy < 0 or xx >= n or yy >= n or arr[xx][yy]) {
+                            check = false;
+                            break;
+                        }
+                    }
+                }
+                if (check) {
+                    go({x, y}, i);
+                }
+            }
+        }
+    }
+
+}
+
+int main() {
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    go({0, 1}, 1);
+    printf("%d", ans);
+}
