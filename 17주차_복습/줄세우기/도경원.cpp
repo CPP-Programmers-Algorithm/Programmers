a52229faa46f4c02071eeb13ddab068bf83ad98a
fortress43-dev <81908835+fortress43-dev@users.noreply.github.com>
2023-04-30 21:53:30 +0900

    17주차 도경원

---
+++ 17주차_복습/줄세우기/도경원.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+vector<int> adj[32001];
+int deg[32001];
+int n, m;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	while (m--) {
+		int a, b;
+		cin >> a >> b;
+		adj[a].push_back(b); // a에 연결된 정점정보를 저장
+		deg[b]++; //indegree의 수를 높여준다
+	}
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
+		if (deg[i] == 0) q.push(i); //indegree가 없는 정점을 q에 넣는다
+	}
+	while (!q.empty()) {
+		int cur = q.front(); q.pop(); // indegree가 0인 정점을 가져온다
+		cout << cur << ' ';
+		for (int nxt : adj[cur]) { //해당정점에 연결된 정점정보를 가져운다
+			deg[nxt]--; //연결된 정점의 indegree를 하나 줄이고
+			if (deg[nxt] == 0) q.push(nxt); //그랬을 때 부모노드가 하나도 없어지면 q에 넣는다
+		}
+	}
+}
